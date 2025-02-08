decad287e75248bafcb0624f6c5f6782893f6a84
Henry Roeth <henrykroeth@gmail.com>
2025-02-08 15:24:04 -0500

    Phase 1 initial commit

---
+++ src/phase_1_testing.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+
+# Load a pretrained YOLO11n model
+model = YOLO("yolo11n-cls.pt")
+
+# Define path to the image file
+source = "C:/Users/henry/cpsc386-cv-project/data/closeup-shot-black-labrador-playing-grass-surrounded-by-greenery.jpg"
+
+# Run inference on the source
+results = model(source) # list of Results objects
+
+# Access first element of the results list
+result = results[0]
+
+# Display the result
+result.show()
